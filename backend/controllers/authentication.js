d9b9caea2a1fe87fd754416bf2aebad8066bf970
Eshita Islam <islam.eshita@gmail.com>
2023-09-16 08:49:06 -0700

    Cleaned up code

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -9,15 +9,29 @@ router.post("/", async (req, res) => {
     where: { email: req.body.email },
   });
 
-  if (
-    !user ||
-    !(await bcrypt.compare(req.body.password, user.passwordDigest))
-  ) {
+  let msg = "";
+
+  if (!user) {
+    msg = `Could not find a user with the provided username`;
     res.status(404).json({
-      message: `Could not find a user with the provided username and password`,
+      message: msg,
     });
   } else {
-    res.json({ user });
+    let passwordMatch = await bcrypt.compare(
+      req.body.password,
+      user.passwordDigest
+    );
+    if (!passwordMatch) {
+      msg = `Password did not match!`;
+      console.log("password mismatch");
+      res.status(404).json({
+        message: msg,
+      });
+    } else {
+      console.log("Successfully authenticated!");
+      console.log(user);
+      res.status(200).json({ user });
+    }
   }
 });
 
